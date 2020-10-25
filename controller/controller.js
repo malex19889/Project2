50844fcd055d47c124c45215985cbe1429305ef4
malex19889 <malex19889@yahoo.com>
2020-10-25 12:20:51 -0500

    updated eslintrc for es6 format

--- controller/controller.js
+++ controller/controller.js
@@ -4,11 +4,11 @@ const express = require("express");
 // Create the router for the app
 const router = express.Router();
 //
-router.get("/", function (req, res) {});
+router.get("/", function (req, res) { });
 // listener for new burger
-router.post("/api/", function (req, res) {});
+router.post("/api/", function (req, res) { });
 //  listener for burger status update
-router.put("/api//:id", function (req, res) {});
+router.put("/api//:id", function (req, res) { });
 
 // export thr express router
 module.exports = router;
