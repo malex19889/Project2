7a00b5f24b62a156f3447c2ded6cc8f3fc2c3d6e
cascutter <cascutter13@gmail.com>
2020-10-28 18:03:14 -0500

    Boilerplate code changed to REcords

---
+++ models/records.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+    var Records = sequelize.define("Records", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        }
+    });
+
+    return Post;
+};
