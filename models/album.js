efc7e3bf88f27092f4c5839e1170ee41cb5d1f8c
cascutter <cascutter13@gmail.com>
2020-10-28 21:30:49 -0500

    Sequelize model for album table

---
+++ models/album.js
@@ -0,0 +1,65 @@
+/* eslint-disable indent */
+module.exports = function(sequelize, DataTypes) {
+    var Album = sequelize.define("Album", {
+        albumName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        artist: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        releaseYear: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        albumArt: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cd: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        cassette: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        vinylSeven: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        vinylTwelve: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        eightTrack: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        digital: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        condition: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        favorite: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+    });
+    return Album;
+};
