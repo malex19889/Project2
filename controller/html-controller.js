f9632237681a5235b12fc6d78f7bd518aa92d8e7
malex19889 <malex19889@yahoo.com>
2020-10-29 22:55:50 -0500

    adding new routing

---
+++ controller/html-controller.js
@@ -0,0 +1,33 @@
+
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+  // route to index
+  app.get("/", function(req, res) {
+    res.render("index");
+  });
+  // route for registering a new user
+  app.get("/register", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("register");
+  });
+  // login route
+  app.get("/login", function(req, res) {
+    // If the user is already logged in send them back to members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("login");
+  });
+
+  // If a user who is not logged in tries to access this route they will be redirected to the login page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.render("collection");
+  });
+
+};
