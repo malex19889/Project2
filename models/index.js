69fd060d17bdf253e2aa69e2935ebc4c450fea57
malex19889 <malex19889@yahoo.com>
2020-11-03 23:30:27 -0600

    updating db settings to work with production db

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, process.env.MYSQLPW, config);
+  sequelize = new Sequelize(process.env.Db, process.env.USER, process.env.MYSQLPW, config);
 }
 
 fs
